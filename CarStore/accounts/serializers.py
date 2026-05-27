52f830bae85e338ee285dd11e240dc1a9442d90b
Sergey Filyakovskiy <veteran0velikiy@gmail.com>
2026-05-27 16:13:53 +0300

    added custom serializers and views for JWT

---
+++ CarStore/accounts/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = cls.get_token(user)
+        token["role"] = user.role
+        token["username"] = user.username
+
+        return token
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
--- CarStore/accounts/urls.py
+++ CarStore/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
+from .serializers import CustomTokenObtainPairView
 from .views import login_view, signup_view
 
 urlpatterns = [
     path("/signup", signup_view, name="signup"),
     path("/login", login_view, name="login"),
-    path("/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("/token", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path(
         "/token/refresh",
         TokenRefreshView.as_view(),
