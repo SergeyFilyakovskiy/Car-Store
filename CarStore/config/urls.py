c8dfaeb863185ed712352b6760caab469d2a769e
Sergey Filyakovskiy <veteran0velikiy@gmail.com>
2026-07-31 22:17:00 +0300

    add car app in main urls add car app in settings

--- CarStore/config/urls.py
+++ CarStore/config/urls.py
@@ -25,6 +25,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("api/", include("accounts.urls")),
+    path("api/", include("cars.urls")),
     path("admin/", admin.site.urls),
     path("api/schema", SpectacularAPIView.as_view(), name="schema"),
     path(
