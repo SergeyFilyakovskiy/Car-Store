bf293f7a112708e05cdcb5082153e845e125dfae
Sergey Filyakovskiy <veteran0velikiy@gmail.com>
2026-08-29 12:37:44 +0300

    add: custom exceptions for deals operations

---
+++ CarStore/deals/exceptions.py
@@ -0,0 +1,28 @@
+class DealError(Exception):
+    """Base exception for deal opertions."""
+
+    pass
+
+
+class OfferAlreadyProcessedError(DealError):
+    """Offer status is not PENDING"""
+
+    pass
+
+
+class InsufficientBalanceError(DealError):
+    """Buyer doesn't have enough balance."""
+
+    pass
+
+
+class OutOfStockError(DealError):
+    """Dealership has no cars of this model in stock."""
+
+    pass
+
+
+class PriceMismatchError(DealError):
+    """Offer max_price is less than inventory sale_price."""
+
+    pass
