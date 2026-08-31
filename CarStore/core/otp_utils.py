e9d0d3c6ecb20c1e8c4f7b5964b15eda61d1570a
Sergey Filyakovskiy <veteran0velikiy@gmail.com>
2026-08-31 17:39:11 +0300

    add: new functions for sending otp

---
+++ CarStore/core/otp_utils.py
@@ -0,0 +1,47 @@
+import random
+
+from django.conf import settings
+from django.core.cache import cache
+from django.core.mail import send_mail
+
+
+def generate_and_send_otp(user):
+    """Generates a 6-digit code, saves it to the cache, and sends it via email."""
+
+    otp = str(random.randint(100000, 999999))
+
+    otp_key = f"otp_code_{user.id}"
+    attempts_key = f"otp_attempts_{user.id}"
+
+    cache.set(otp_key, otp, timeout=300)
+    cache.set(attempts_key, 0, timeout=300)
+
+    subject = "Your verification code"
+    message = f"Your one-time login code: {otp}\n The code is valid for 5 minutes."
+
+    send_mail(
+        subject, message, settings.DEFAULT_FROM_EMAIL, [user.email], fail_silently=False
+    )
+
+
+def verify_otp(user, entered_otp):
+    otp_key = f"otp_key{user.id}"
+    attempts_key = f"otp_attempts_{user.id}"
+
+    valid_otp = cache.get(otp_key)
+    attempts = cache.get(attempts_key, 0)
+
+    if attempts >= 5:
+        cache.delete(otp_key)
+        return False, "Too many attempts. Code voided; please log in."
+
+    if not valid_otp:
+        return False, "The code has expired or was not found."
+
+    if entered_otp != valid_otp:
+        cache.set(attempts_key, attempts + 1, timeout=300)
+        return False, "Invalid code."
+
+    cache.delete(otp_key)
+    cache.delete(attempts_key)
+    return True, None
