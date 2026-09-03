bd35a6f94b298eafb963329ef6f38d4f5ef54fa9
Sergey Filyakovskiy <veteran0velikiy@gmail.com>
2026-09-03 18:28:15 +0300

    add: new otp utils

--- CarStore/core/otp_utils.py
+++ CarStore/core/otp_utils.py
@@ -45,3 +45,50 @@ def verify_otp(user, entered_otp):
     cache.delete(otp_key)
     cache.delete(attempts_key)
     return True, None
+
+
+def generate_email_change_otp(user):
+    """Generate OTP for email change verification."""
+
+    otp = f"{random.randint(0, 999999): 06d}"
+
+    otp_key = f"email_change_otp_{user.id}"
+    attempts_key = f"email_change_attempts_{user.id}"
+
+    cache.set(otp_key, otp, timeout=300)
+    cache.delete(attempts_key)
+
+    send_mail(
+        subject="Confirm your new email address",
+        message=f"Your conrimation code: {otp}\nValid for 5 minutes.",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user.pending_email],
+        fail_silently=False,
+    )
+
+    return otp
+
+
+def verify_email_change_otp(user, entered_otp):
+    """Verify OTP for email change"""
+
+    otp_key = f"email_change_otp_{user.id}"
+    attempts_key = f"email_change_attempts_{user.id}"
+
+    valid_otp = cache.get(otp_key)
+    attempts = cache.get(attempts_key, 0)
+
+    if attempts >= 5:
+        cache.delete(otp_key)
+        return False, "Too many attempts. Request a new code."
+
+    if not valid_otp:
+        return False, "Code expired or not found. Request a new code."
+
+    if entered_otp != valid_otp:
+        cache.set(attempts_key, attempts + 1, timeout=300)
+        return False, f"Invalid code. Attempts left: {5 - attempts}"
+
+    cache.delete(otp_key)
+    cache.delete(attempts_key)
+    return True, None
